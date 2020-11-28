af2024054453fbdace453ddbb5a00ef1c5aa2671
Daniel Ebert <iuq@web.de>
2020-11-28 15:54:24 -0800

    Clear up service src

--- src/service/service.py
+++ src/service/service.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import requests
 import json
+import os
 
 KEYCLOAK_HOST = 'localhost:8080'
 REALM_NAME = 'Test'
@@ -22,9 +23,8 @@ def check_token(request_headers):
   url = f'http://{KEYCLOAK_HOST}/auth/realms/{REALM_NAME}/protocol/openid-connect/userinfo'
   headers = {"Authorization": f'Bearer {headerAuth[1]}'}
   r = requests.get(url, headers=headers)
-  print(r.text)
   if r.status_code != 200:
-    return None
+    return None 
   r_json = json.loads(r.text)
   return f'subject identifier (sub): {r_json["sub"]}'
 
@@ -33,8 +33,10 @@ def check_token(request_headers):
 def hello():
   user = check_token(request.headers)
   if user is None:
-    return "FAILED", 401
+    return "FAILED", 402
   return user
 
 if __name__ == "__main__":
-  app.run(debug=True, port=5001)
+  if 'PORT' not in os.environ:
+    raise Exception('PORT environment variable required')
+  app.run(host='0.0.0.0', port=os.environ['PORT'], debug=True)
