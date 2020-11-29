9b1a77cf1310c681c5941a9d89f1338077d3aa99
Daniel Ebert <iuq@web.de>
2020-11-28 17:06:31 -0800

    Backend checks if their client_ID is in aud claim

--- src/service/service.py
+++ src/service/service.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, request
 from flask_cors import CORS
+import jwt
 import requests
 import json
 import os
@@ -12,7 +13,11 @@ REALM_NAME = 'Test'
 app = Flask(__name__)
 CORS(app)
 
-def check_token(request_headers):
+if 'client_ID' not in os.environ:
+  raise Exception('client_ID environment variable required')
+client_ID = os.environ['client_ID']
+
+def extract_token(request_headers):
   if not 'Authorization' in request_headers:
     return None
   headerAuth = request_headers['Authorization'].split()
@@ -20,21 +25,32 @@ def check_token(request_headers):
     return None
   if headerAuth[0] != "Bearer":
     return None
+  return headerAuth[1]
+
+def validate_token(token):
+  # Decode Access Token without validating it.
+  # Validation will be done via Keycloak.
+  decoded_token = jwt.decode(token, verify=False)
+  # Invalid if this client's ID is not in 'aud' claim
+  if 'aud' not in decoded_token:
+    return 'FAILED: no "aud" claim in token', 402
+  if client_ID not in decoded_token['aud']:
+    return f'FAILED: {client_ID} not in "aud" claim', 402
   url = f'http://{KEYCLOAK_HOST}/auth/realms/{REALM_NAME}/protocol/openid-connect/userinfo'
-  headers = {"Authorization": f'Bearer {headerAuth[1]}'}
+  headers = {"Authorization": f'Bearer {token}'}
   r = requests.get(url, headers=headers)
   if r.status_code != 200:
-    return None 
+    return 'Invalid signature', 402
   r_json = json.loads(r.text)
-  return f'subject identifier (sub): {r_json["sub"]}'
+  return f'subject identifier (sub): {r_json["sub"]}', 200
 
 
 @app.route("/", methods = ['POST'])
 def hello():
-  user = check_token(request.headers)
-  if user is None:
+  token = extract_token(request.headers)
+  if token is None:
     return "FAILED", 402
-  return user
+  return validate_token(token)
 
 if __name__ == "__main__":
   if 'PORT' not in os.environ:
